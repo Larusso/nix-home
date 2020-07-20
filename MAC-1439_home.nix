294c3cf41007040b3d6120c2cf60341fe2e7c194
Manfred Endres <manfred.endres@tslarusso.de>
2020-07-20 20:05:07 +0200

    Add home.nix platform files to symlink on host systems

---
+++ MAC-1439_home.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+let
+  larussopkgsTarball = fetchTarball https://github.com/Larusso/larussopkgs/archive/nixexprs.tar.gz;
+in
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      larussopkgs = import larussopkgsTarball {
+        system = config.nixpkgs.system;
+      };
+    };
+  };
+  programs.home-manager.enable = true;
+  home.stateVersion = "19.09";
+  imports = [
+    ./user/larusso
+    ./machine/MAC-1439
+    ./role/darwin
+  ];
+}
--- machine/ripper/default.nix
+++ machine/ripper/default.nix
@@ -1,7 +1,43 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [
+    ../../program/terminal/aws
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
   home.packages = [
     pkgs.git-crypt
+
+    pkgs.slack-dark
+
+    pkgs.awscli
+    pkgs.heroku
+
+    # ssh
+    pkgs.sshpass
+
+    # gradle
+    pkgs.gradle_4
+    pkgs.gradle-completion
+    pkgs.larussopkgs.gdub
+    pkgs.adoptopenjdk-hotspot-bin-8
+
+    # unity
+    pkgs.mono6
   ];
+
+  programs.zsh = {
+    sessionVariables = {
+      UVM_AUTO_SWITCH_UNITY_EDITOR = "YES";
+      UVM_AUTO_INSTALL_UNITY_EDITOR = "YES";
+      UVM_UNITY_INSTALL_BASE_DIR= "/Applications/Unity/Hub/Editor";
+      ANDROID_SDK_ROOT = "/usr/local/share/android-sdk";
+    };
+
+    shellAliases = {
+      gradle = "gw";
+    };
+  };
 }
